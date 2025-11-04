26f72ff17f6f14c0ea97899dae8f414387a0a7b1
Sosality <96949675+Sosality@users.noreply.github.com>
2025-11-04 10:45:25 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 import pg from "pg";
 const { Pool } = pg;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://svoychatdb_user:tK11hGJtTLbkDMsneRJaei0WpD8rgkj0@dpg-d44qsf0dl3ps73bi5g10-a:5432/users';
+const connectionString = process.env.DATABASE_URL || 'postgresql://svoychatdb_user:tK11hGJtTLbkDMsneRJaei0WpD8rgkj0@dpg-d44qsf0dl3ps73bi5g10-a:5432/svoychatdb';
 
 export const pool = new Pool({
   connectionString,
